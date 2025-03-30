eca35db7fb96118ad7833e88e9cdf3fe94ee076a
Gani0230 <ganeshjadhav7478@gmail.com>
2025-03-30 23:32:08 +0530

    modify todo in useEffect

--- week6/6.1/todo_useEffect/src/App.jsx
+++ week6/6.1/todo_useEffect/src/App.jsx
@@ -1,29 +1,44 @@
 import { useEffect } from "react"
 import { useState } from "react"
 
+let count = 0
 function App() {
   const [todos, setTodos] = useState()
-  const [count, setCount] = useState(0)
 
   useEffect(()=>{
     const fechtodos = ()=>{
       fetch("https://dummyjson.com/todos").then(async (res)=>{
         const json = await res.json()
-        setCount(count + 1)
         setTodos(json.todos[count])
+        count++
         console.log(count)
       })
     }
     fechtodos()
-    setInterval(fechtodos, 3000)
+    const interval = setInterval(fechtodos, 3000)
+    return () => clearInterval(interval)
+     
+    
     // useEffect runs when count changes
   },[])
 
   return (
     <div>
-      {JSON.stringify(todos)}
+      {todos ?(
+        <div>
+        <h3>ID: {todos.id}</h3>
+        <p>Task: {todos.todo}</p>
+        <p>Status: {todos.completed ? "✅ Completed" : "❌ Not Completed"}</p>
+        </div>
+      ): (
+        <p>process...</p>
+      )}
     </div>
+      
+
+          
   )
+  
 }
 
 export default App
