17d49a3edf030defa6d70a8cd5569921dbbb2154
Gani0230 <ganeshjadhav7478@gmail.com>
2025-06-25 12:58:53 +0530

    noImplicitAny use case in ts

---
+++ week9/9.3 emuns_generics/src/implicit_any.ts
@@ -0,0 +1,10 @@
+// If true this will throw error
+//  "noImplicitAny": true,     /* Enable error reporting for expressions and declarations with an implied 'any' type. */
+
+
+function add(a, b){
+    return a+b
+}
+
+const result = add(1,2)
+console.log(result)
