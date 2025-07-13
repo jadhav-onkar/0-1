4315acd82bd5de9bf122ac51a3b3a7d23334ecdc
Turbobot <turbobot@vercel.com>
2025-07-13 11:15:15 +0530

    feat(create-turbo): apply official-starter transform

--- week12/own_packages/week-8-repo/client/src/Components/Signup.tsx
+++ week12/own_packages/week-8-repo/client/src/Components/Signup.tsx
@@ -2,22 +2,25 @@ import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {useSetRecoilState} from "recoil";
 import {authState} from "../store/authState.js";
+import { userParams } from '@ganesh0230/common'
 
 const Signup = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
+    const user:userParams = {
+        username,
+        password
+    }
     const handleSignup = async () => {
         const response = await fetch('http://localhost:3000/auth/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
+            body: JSON.stringify(user)
         });
         // Todo: Create a type for the response that you get back from the server
         const data = await response.json();
         if (data.token) {
             localStorage.setItem("token", data.token)
-            window.location = "/todos";
         } else {
             alert("Error while signing up");
         }
