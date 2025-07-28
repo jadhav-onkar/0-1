4a0491bf53712548f4c741fd98fa36184a8a9943
Gani0230 <ganeshjadhav7478@gmail.com>
2025-07-28 19:21:49 +0530

    postgres in next

--- week14/backend_next/app/api/user/route.ts
+++ week14/backend_next/app/api/user/route.ts
@@ -1,3 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest } from "next/server";
+
+
+const client = new PrismaClient()
 
 export function GET(){
     return Response.json({
@@ -5,3 +10,18 @@ export function GET(){
         email:"ganesh@gmail.com"
     })
 }
+
+export async function POST(req:NextRequest){
+    const body = await req.json();
+    const user = await client.user.create({
+        data:{
+            email:body.username,
+            password:body.password
+        }
+    })
+
+    return Response.json({
+        user,
+        msg:"you are signed in succesfully"
+    })
+}   
